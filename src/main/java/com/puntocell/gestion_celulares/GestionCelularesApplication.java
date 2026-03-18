7c65c5eb0486c9ff84b97b3562751d946c0c6719
Camilo Gallego <c.gallego3029@gmail.com>
2026-03-18 09:52:16 -0500

    base del proyecto

---
+++ src/main/java/com/puntocell/gestion_celulares/GestionCelularesApplication.java
@@ -0,0 +1,13 @@
+package com.puntocell.gestion_celulares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCelularesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCelularesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puntocell/gestion_celulares/GestionCelularesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puntocell.gestion_celulares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCelularesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
